469b5dd7fcfcb4681a2adc79978399ed0cabbacc
rakasyau <rakasyauqiferdian@gmail.com>
2025-12-09 23:54:04 +0700

    update providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if($this->app->environment('production')) {
-            \URL::forceScheme('https');
+            URL::forceScheme('https');
         }
     }
 
