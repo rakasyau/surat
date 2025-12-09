3256fe5df4384cb7c11a7e988058a4f75d2e6aea
rakasyau <rakasyauqiferdian@gmail.com>
2025-12-10 00:09:57 +0700

    update bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'is.admin' => \App\Http\Middleware\IsAdmin::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
